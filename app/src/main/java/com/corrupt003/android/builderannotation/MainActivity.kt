12bcee3e365ab34e9e751f02313635ba93faa746
yuan_chen <corrupt003@gmail.com>
2019-10-25 17:36:47 +0800

    Let Java and Kotlin code can use generated java class.
    Add Kotlin annotation processor plugin, and use it to generate Java class.

--- app/src/main/java/com/corrupt003/android/builderannotation/MainActivity.kt
+++ app/src/main/java/com/corrupt003/android/builderannotation/MainActivity.kt
@@ -2,11 +2,20 @@ package com.corrupt003.android.builderannotation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val user = UserBuilder()
+            .name("corrupt003")
+            .age(18)
+            .build()
+
+        val message = "User name: ${user.name}, age: ${user.age}"
+        main_message.text = message
     }
 }
---
+++ app/src/main/java/com/corrupt003/android/builderannotation/MainJavaActivity.java
@@ -0,0 +1,29 @@
+package com.corrupt003.android.builderannotation;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Locale;
+
+public class MainJavaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_java);
+
+        User user = new UserBuilder()
+                .name("corrupt003")
+                .age(20)
+                .build();
+
+        String text = String.format(
+                Locale.getDefault(),
+                "User name: %s, age: %d",
+                user.name, user.age);
+
+        TextView message = findViewById(R.id.main_java_message);
+        message.setText(text);
+    }
+}
